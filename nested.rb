649cad34a77af5ce0b6b01562679030199be108e
bryan lesht <bryan.lesht@colorado.edu>
2020-01-29 23:43:31 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -129,10 +129,10 @@ def adding_to_dennis
         },
         :dennis_ritchie => {
           :known_for => "Unix",
-          :languages => ["C"]
+          :languages => ["C","Assembly"]
         }
      }
-     programmer_hash[:dennis_ritchie][:languages] = ["C","Assembly"]
+     # programmer_hash[:dennis_ritchie][:languages] = ["C","Assembly"]
      programmer_hash[:dennis_ritchie][:languages]
 
 end
